9028712ba336f7a31211a66696bea3053096ec9e
PsyBrainy <martuto93@gmail.com>
2021-03-25 12:01:40 -0300

    init

---
+++ src/main/java/com/psybrainy/alkemychallenge/AlkemyChallenge3Application.java
@@ -0,0 +1,13 @@
+package com.psybrainy.alkemychallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlkemyChallenge3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlkemyChallenge3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/psybrainy/alkemychallenge/AlkemyChallenge3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psybrainy.alkemychallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlkemyChallenge3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
